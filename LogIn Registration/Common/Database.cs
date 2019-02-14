9b5df0ff395f9b96f0b9ee1eca71f928763493f5
Srikanth Kundrapu <srikanth.kundrapu@sls.ads.valuelabs.net>
2019-02-14 19:24:40 +0530

    Add project files.

---
+++ LogIn Registration/Common/Database.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net;
+
+namespace LogIn_Registration.Common
+{
+   public class Database
+    {
+        string path;
+        SQLiteConnection conn;
+
+        public Database()
+        {
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "MyDatabase.sqlite");
+            conn = new SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(),path);
+            conn.CreateTable<User>();
+        }
+        public int Register(User user)
+        {
+            int code = 1;
+            try
+            {
+                conn.Insert(new User()
+                {
+                    UserName = user.UserName,
+                    Password = user.Password,
+                    Email = user.Email
+                });
+            }
+            catch(SQLiteException ex)
+            {
+                code = -1;
+            }
+            return code;
+        }
+        public bool Login(string user, string password)
+            {
+                var query = conn.Table<User>().Where(t => t.UserName == user && t.Password == password);
+            if (query.Count() > 0)
+                return true;
+            else
+                return false;
+            }
+        }
+    }
+
---
+++ LogIn Registration/RegisterPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using LogIn_Registration.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace LogIn_Registration
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class RegisterPage : Page
+    {
+        Database db;
+        public RegisterPage()
+        {
+            this.InitializeComponent();
+            db= new Database();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            SystemNavigationManager.GetForCurrentView().BackRequested += RegisterPage_BackRequested;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            SystemNavigationManager.GetForCurrentView().BackRequested -= RegisterPage_BackRequested;  
+        }
+
+        private void RegisterPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+                Frame.GoBack();
+        }
+
+
+        private async void btnRegister_Click(object sender,RoutedEventArgs e)
+
+        {
+          int code = db.Register(new Common.User()
+            {
+                UserName = txtUserName.Text.Trim(),
+                Password = txtPassword.Password,
+                Email = txtEmail.Text.Trim()
+            }
+                );
+            if(code == -1)
+            {
+                var message = new MessageDialog("RegisterFailed");
+                await message.ShowAsync();
+            }
+            else
+            {
+                var message = new MessageDialog("RegisterSuccess");
+                await message.ShowAsync();
+            }
+        }
+
+   
+    }
+}
